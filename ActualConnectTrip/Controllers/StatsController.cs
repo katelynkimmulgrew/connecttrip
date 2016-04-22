8bbb022ad9c8b42baf099649b7d6172515d8eee1
Joanna Crystal Serrao <jcs771@nyu.edu>
2016-04-22 01:25:37 -0700

    StatsController.cs throws an error which has been commented.

--- ActualConnectTrip/Controllers/StatsController.cs
+++ ActualConnectTrip/Controllers/StatsController.cs
@@ -5,16 +5,20 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using ActualConnectTrip.Models;
-
+using DataLayer;
 namespace ActualConnectTrip.Controllers
 {
     public class StatsController : Controller
     {
+        private Entities db = new Entities();
         // GET: Stats
         public ActionResult Index()
         {
+            var currentUserId = User.Identity.GetUserId();
+            //The below line throws an error
+           // ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);   
             //get the dbContext???!!!!-> should a new one be created or DataLayer.Entities.cs is the dbContext to be used
-            string currentUserId = User.Identity.GetUserId();
+          
            //get the user
            //assign the values to the view model object
           /* var model=new StatsViewModel()
