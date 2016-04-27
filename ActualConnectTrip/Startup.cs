0f55fbfef8f7d6cb1b4a9d7bb6e32b2be142fcf8
Katelyn Mulgrew <ksm348@nyu.edu>
2016-04-27 10:51:28 -0400

    set up class libraries

---
+++ ActualConnectTrip/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ActualConnectTrip.Startup))]
+namespace ActualConnectTrip
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
