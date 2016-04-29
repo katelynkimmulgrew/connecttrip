7c027776568f38298b99da8016586e8e4274ec77
Katelyn Mulgrew <ksm348@nyu.edu>
2016-04-29 14:19:55 -0400

    startGamePlayer class added

---
+++ DataLayer/startGamePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class startGamePlayer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int player1Id { get; set; }
+        public int player2Id { get; set; }
+        public bool isStarted { get; set; }
+    }
+}
