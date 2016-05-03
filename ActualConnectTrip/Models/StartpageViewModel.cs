87b7c34c039ecf75d18eeaa276eee209f289e676
qyxiao <qx344@nyu.edu>
2016-05-03 11:11:59 +0800

    creates some files for startpages

---
+++ ActualConnectTrip/Models/StartpageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace ActualConnectTrip.Models
+{
+    public class StartpageViewModel
+    {
+        public int myid { get; set; }
+        public virtual List<startGamePlayer> rivals { get; set; }
+        public int oppoid { get; set;}
+    }
+}
