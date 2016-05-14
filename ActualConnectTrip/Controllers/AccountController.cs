ed9c4647bec0deebdfd3af89db66cb2da62e8d64
Joanna Crystal Serrao <jcs771@nyu.edu>
2016-05-14 14:25:06 -0700

    navigation bar user list

--- ActualConnectTrip/Controllers/AccountController.cs
+++ ActualConnectTrip/Controllers/AccountController.cs
@@ -81,7 +81,7 @@ namespace ActualConnectTrip.Controllers
             {
                 case SignInStatus.Success:
                     //  return RedirectToLocal(returnUrl);
-                    return RedirectToAction("stindex", "Game");
+                    return RedirectToAction("Welcome", "Game");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -193,7 +193,7 @@ namespace ActualConnectTrip.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("stindex", "Game");
+                    return RedirectToAction("Welcome", "Game");
                 }
                 AddErrors(result);
             }
